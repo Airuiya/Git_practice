87ad4a386333c822474574e6cd83d9fc3a55c12f
gmt_kj <gumt0309@163.com>
2024-08-09 18:17:11 +0800

    设置我的模块

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1>引入数据</h1>
-    <h2>{{ myData }}</h2>
+    <!-- <h1>引入数据</h1>
+    <h2>{{ myData }}</h2> -->
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -10,8 +10,10 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+          rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+          rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -25,7 +27,8 @@
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
+      </li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
@@ -33,11 +36,14 @@
 </template>
 
 <script>
-import { dataInJs } from "../data/dataInJs";
+import { dataInJs } from "../assets/data/dataInJs";
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      default: ''
+    }
   },
   data() {
     return {
@@ -52,14 +58,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
---
+++ src/components/MyModule.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="my-module">
+        <h2>我开发的页面</h2>
+        <h4 @click="toLottery">抽奖功能</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyModule',
+    data() {
+        return {
+            url: '',
+        }
+    },
+    methods: {
+        toLottery() {
+            console.log('点击去抽奖页面');
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.my-module {
+    background-color: skyblue;
+    margin: 0 auto;
+    width: 400px;
+    padding: 20px;
+    border: 1px solid #4573d5;
+    border-radius: 10px;
+
+}
+</style>
