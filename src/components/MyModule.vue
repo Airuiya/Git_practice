5de18e2bb667fba89c920687f145ec15adccff3d
gmt_kj <gumt0309@163.com>
2024-08-13 19:43:38 +0800

    侧边导航栏

--- src/components/MyModule.vue
+++ src/components/MyModule.vue
@@ -1,35 +1,89 @@
 <template>
     <div class="my-module">
-        <h2>我开发的页面</h2>
-        <h4 @click="toLottery">抽奖功能</h4>
+        <!-- <h2>我开发的页面</h2> -->
+        <!-- <h4 @click="toLottery">抽奖功能</h4> -->
+        <el-container style="height: 100%; border: 1px solid #eee">
+            <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+                background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" style="min-width: 200px;;">
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>学习vue</span>
+                    </template>
+                    <el-menu-item index="1-1">8.13 路由</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-submenu>
+                <el-submenu index="2">
+                    <template slot="title">
+                        <i class="el-icon-menu"></i>
+                        <span>我的开发</span>
+                    </template>
+                    <el-menu-item index="2-1">抽奖功能</el-menu-item>
+                    <el-menu-item index="2-2">选项2</el-menu-item>
+                </el-submenu>
+                <el-menu-item index="3" disabled>
+                    <i class="el-icon-document"></i>
+                    <span slot="title">导航三</span>
+                </el-menu-item>
+            </el-menu>
+            <!-- <el-aside width="180px" height="100%" style="background-color: #545c64;">
+            </el-aside> -->
+
+            <el-container>
+                <el-header height="50px" class="my-header">学习快乐</el-header>
+                <el-main height="400px" class="my-center"></el-main>
+                <el-footer class="my-footer"></el-footer>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'MyModule',
+    name: "MyModule",
     data() {
         return {
-            url: '',
-        }
+            url: "",
+            activeIndex: 1,
+        };
     },
     methods: {
         toLottery() {
-            console.log('点击去抽奖页面');
-        }
-    }
-}
+            console.log("点击去抽奖页面");
+        },
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less" scoped>
 .my-module {
-    background-color: skyblue;
-    margin: 0 auto;
-    width: 400px;
-    padding: 20px;
-    border: 1px solid #4573d5;
-    border-radius: 10px;
+    width: 100%;
+    height: 520px;
+
+    .my-header {
+        font-size: 24px;
+        line-height: 50px;
+        background: #545c64;
+    }
+
+    .el-menu-vertical-demo {
+        // width: 180px;
+    }
+
+    .my-center {
+        background: #fff;
+    }
+
+    .my-footer {
+        background: #ccc;
+    }
 
 }
 </style>
